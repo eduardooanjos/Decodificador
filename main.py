a1fe978152a56df17faa551249709cac0ee50e41
eduardooanjos <eduardo-henrique-rocha@hotmail.com>
2026-02-25 11:15:28 -0300

    ajuste

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-from bin import to_bin
-from hex import to_hex
+numbers = []
+
+with open("text.txt", "r", encoding="utf-8") as f:
+    for line in f:
+        line = line.strip()
+        if line:
+            numbers.append(bin(int(line))[2:]) 
+
+print(numbers)
