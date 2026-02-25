a5d654266411fa038447a2c8f15375831366e566
eduardooanjos <eduardo-henrique-rocha@hotmail.com>
2026-02-25 12:16:50 -0300

    add bin, hex, oct functions to main.py

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 numbers = []
+binary = []
+hexadecimal = []
 
 with open("text.txt", "r", encoding="utf-8") as f:
     for line in f:
         line = line.strip()
         if line:
-            numbers.append(bin(int(line))[2:]) 
+            n = int(line)
+            numbers.append(n)          
+            binary.append(bin(n)[2:]) 
+            hexadecimal.append(hex(n)[2:])  
 
-print(numbers)
+# print(numbers)
+# print(binary)
+print(hexadecimal)
